1e43fb7f36469e8ac7c05d961997153ee729d17b
jeanne <jeanne.lefevre@hotmail.fr>
2022-12-05 17:27:39 +0100

    profile page witdh eventsmallcards components

--- src/components/Profile/SubscribedEvents.jsx
+++ src/components/Profile/SubscribedEvents.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react"
 import service from "../../api/apiHandler"
+import EventCardSmall from "../Events/EventCardSmall";
 import { Link } from "react-router-dom";
 
 
@@ -19,47 +20,11 @@ function SubscribedEvents() {
     }, [])
     if (!events) return <div className="nothing-to-show">Nothing to show....</div>
     return (
-        events.map((element) => {
-            return (
-                <Link key={element._id} to={`/events/${element._id}`}>
-                <div className="event-card">
-                  <picture>
-                    <img src={element.image} alt="event picture" />
-                  </picture>
-                  <div className="event-content">
-                    <p className="keywords">Keywords: {element.keywords}</p>
-                    <h2>{element.title}</h2>
-                    <div className="event-info">
-                      <div>
-                        <i className="fa fa-calendar" aria-hidden="true"></i>
-                        <span>
-                          {new Intl.DateTimeFormat("en-GB").format(
-                            new Date(element.dateOfEvent)
-                          )}
-                        </span>
-                      </div>
-                      <div>
-                        <i className="fa-regular fa-clock"></i>
-                        <span>{element.time}</span>
-                      </div>
-                      <div>
-                        <i className="fa-solid fa-location-dot"></i>
-                        <span>{element.location}</span>
-                      </div>
-                      <div>
-                        <i className="fa fa-eur" aria-hidden="true"></i>
-                        <span>{element.price || "Free"}</span>
-                      </div>
-                      <div>
-                        <i className="fa fa-list" aria-hidden="true"></i>
-                        <span>{element.category}</span>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </Link>
-            )
-        })
+      <div>
+        {events.map((event) => {
+          return <EventCardSmall key={event._id} event={event} />
+        })}
+      </div>
     )
 }
 
--- src/components/Profile/UserEvents.jsx
+++ src/components/Profile/UserEvents.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react"
 import service from "../../api/apiHandler"
-import { Link } from "react-router-dom";
+import EventCardSmall from "../Events/EventCardSmall";
+
 
 
 function UserEvents() {
@@ -10,7 +11,7 @@ function UserEvents() {
         .getEventsCreatedByUser()
         .then((response) => {
             setEvents(response)
-            console.log(response)
+            //console.log(response)
         })
         .catch((err) => {
             console.error(err.message);
@@ -19,47 +20,11 @@ function UserEvents() {
     }, [])
     if (!events) return <div className="nothing-to-show">Nothing to show....</div>
     return (
-        events.map((element) => {
-            return (
-                <Link to={`/events/${element._id}`}>
-                <div className="event-card">
-                  <picture>
-                    <img src={element.image} alt="event picture" />
-                  </picture>
-                  <div className="event-content">
-                    <p className="keywords">Keywords: {element.keywords}</p>
-                    <h2>{element.title}</h2>
-                    <div className="event-info">
-                      <div>
-                        <i className="fa fa-calendar" aria-hidden="true"></i>
-                        <span>
-                          {new Intl.DateTimeFormat("en-GB").format(
-                            new Date(element.dateOfEvent)
-                          )}
-                        </span>
-                      </div>
-                      <div>
-                        <i className="fa-regular fa-clock"></i>
-                        <span>{element.time}</span>
-                      </div>
-                      <div>
-                        <i className="fa-solid fa-location-dot"></i>
-                        <span>{element.location}</span>
-                      </div>
-                      <div>
-                        <i className="fa fa-eur" aria-hidden="true"></i>
-                        <span>{element.price || "Free"}</span>
-                      </div>
-                      <div>
-                        <i className="fa fa-list" aria-hidden="true"></i>
-                        <span>{element.category}</span>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </Link>
-            )
-         })
+      <div>
+      {events.map((event) => {
+        return <EventCardSmall key={event._id} event={event} />
+      })}
+    </div>
     )
 }
 
