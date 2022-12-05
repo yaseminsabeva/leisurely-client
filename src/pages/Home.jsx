af79085d866bd9b1d119fc0305c258dbe0cfdb95
jeanne <jeanne.lefevre@hotmail.fr>
2022-12-05 12:16:42 +0100

    basic search request backend

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import SearchHome from "../components/Search/SearchHome";
 
 const Home = () => {
 	return (
 		<div>
 			<h1>Welcome 🏡</h1>
+			<SearchHome/>
 		</div>
 	);
 };
