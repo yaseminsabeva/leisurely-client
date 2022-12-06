9ba06461bf766fd7870e2285eea2e873b3538c06
Yasemin Sabeva <yaseminsabeva@yahoo.com>
2022-12-06 15:48:14 +0100

    styling home

--- src/components/Search/SearchHome.jsx
+++ src/components/Search/SearchHome.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import service from "../../api/apiHandler";
+import "./SearchHome.css";
 
 function SearchHome() {
   const [search, setSearch] = useState("");
@@ -10,19 +11,21 @@ function SearchHome() {
     navigate("/events?title=" + search);
   }
   return (
-    <div>
+    <div className="home-search-bar">
       <form onSubmit={handleSearch}>
         <input
           type="search"
           name="search"
           id="search"
+          className="search-input"
+          placeholder="Search for events..."
           value={search}
           onChange={(e) => {
             console.log(e.target.value);
             setSearch(e.target.value);
           }}
         />
-        <button>Search</button>
+        <button className="home-search-btn">Search</button>
       </form>
     </div>
   );
