5add23e20fbee1f28c20f5f1f453450a5639838f
Yasemin Sabeva <yaseminsabeva@yahoo.com>
2022-12-08 09:32:30 +0100

    fixed description

--- src/components/Profile/UserProfile.jsx
+++ src/components/Profile/UserProfile.jsx
@@ -1,25 +1,40 @@
-import React from "react"
-//import useAuth from "../auth/useAuth"
+import React from "react";
 
-
-function UserProfile({user, currentUser}) {
-//const {currentUser} = useAuth()
-
-return (
-	<div className="event-card">
-    <picture><img src={user.picture} alt=""/></picture>
+function UserProfile({ user, currentUser }) {
+  return (
+    <div className="event-card">
+      <picture>
+        <img src={user.picture} alt="" />
+      </picture>
       <div className="profile user-info">
         <div>
-          <div>{user === currentUser ? <h2>Welcome {user.name}</h2> : <h2>{user.name}'s page</h2>}</div>
+          <div>
+            {user === currentUser ? (
+              <h2>Welcome {user.name}</h2>
+            ) : (
+              <h2>{user.name}'s page</h2>
+            )}
+          </div>
           <br></br>
-          <div>Username: <span>{user.username}</span></div>
-          <div>Email: <span>{user.email}</span></div>
+          <div>
+            Username: <span>{user.username}</span>
+          </div>
+          <div>
+            Email: <span>{user.email}</span>
+          </div>
           <br></br>
-          <div>Description: <span>{user.description}</span></div>
+          <div>
+            Description:{" "}
+            <span>
+              {user.description
+                ? user.description
+                : "This user hasn't added a description yet"}
+            </span>
+          </div>
         </div>
       </div>
-	</div>
-  )
+    </div>
+  );
 }
 
-export default UserProfile
+export default UserProfile;
